f4c4a63ae1d4c6d82a9f3e1c17f6b97d301ef758
정순우 <soonwoo2714@gmail.com>
2018-04-19 15:15:40 +0900

    2018_04_19

--- UIPickerView/ViewController.swift
+++ UIPickerView/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
+    @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if component == 0 {
+            return 10
+        }else {
+            return 100
+        }
     }
+    //Picker 에 값 넣기
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "comp01 \(row)"
+        }else{
+            return "comp02 \(row)"
+        }
 
-
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            myLabel.text = "comp01 \(row)"
+        } else {
+            myLabel.text = "comp02 \(row)"
+        }
+    }
 }
-
